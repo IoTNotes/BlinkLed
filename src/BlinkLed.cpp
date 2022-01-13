ed2c3a4c7a523c634ec0861e4a3ebdbd453c4c1c
Marco Cogo <marco.cogo@outlook.it>
2022-01-13 16:09:57 +0100

    Created repository

--- src/BlinkLed.cpp
+++ src/BlinkLed.cpp
@@ -1,10 +1,39 @@
-/*
- Name:		BlinkLed.cpp
- Created:	13/01/2022 15:51:11
- Author:	marco
- Editor:	http://www.visualmicro.com
-*/
-
 #include "BlinkLed.h"
 
+BlinkLed::BlinkLed(uint8_t pin, bool isInverted) {
+	__pin = pin;
+	__inverted = isInverted;
 
+	pinMode(__pin, OUTPUT);
+	digitalWrite(__pin, __inverted ? HIGH : LOW);
+	__state = OFF;
+};
+
+void BlinkLed::blink(uint32_t timeON, uint32_t timeOFF) {
+	if (!timeOFF)
+		timeOFF = timeON;
+
+	uint32_t timeToWait = (__state == ON) ? timeON : timeOFF;
+	uint32_t now = millis();
+	if (now - __lastBlink > timeToWait) {
+		__lastBlink = now;
+		toggle();
+	}
+};
+
+void BlinkLed::on() {
+	digitalWrite(__pin, __inverted ? LOW : HIGH);
+	__state = ON;
+}
+
+void BlinkLed::off() {
+	digitalWrite(__pin, __inverted ? HIGH : LOW);
+	__state = OFF;
+}
+
+void BlinkLed::toggle() {
+	if (__state == ON)
+		off();
+	else
+		on();
+}
--- src/BlinkLed.h
+++ src/BlinkLed.h
@@ -1,18 +1,29 @@
-/*
- Name:		BlinkLed.h
- Created:	13/01/2022 15:51:11
- Author:	marco
- Editor:	http://www.visualmicro.com
-*/
-
-#ifndef _BlinkLed_h
-#define _BlinkLed_h
+#ifndef _BLINK_LED_H
+#define _BLINK_LED_H
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#include "Arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
+
+class BlinkLed {
+private:
+	enum ledState { OFF, ON };
+	uint32_t __lastBlink;
+	uint8_t __pin;
+	bool __inverted;
+	ledState __state;
+
+public:
+	BlinkLed(uint8_t pin, bool isInverted = false);
+	~BlinkLed() {};
+
+	void blink(uint32_t timeON, uint32_t timeOFF = 0);
+	void on();
+	void off();
+	void toggle();
+};
 
 
 #endif
