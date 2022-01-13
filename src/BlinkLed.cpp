a66b80696bdc9fbb9754ceefc0d86063c677a6ab
Marco Cogo <marco.cogo@outlook.it>
2022-01-13 16:02:49 +0100

    Aggiungere i file di progetto.

---
+++ src/BlinkLed.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		BlinkLed.cpp
+ Created:	13/01/2022 15:51:11
+ Author:	marco
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "BlinkLed.h"
+
+
---
+++ src/BlinkLed.h
@@ -0,0 +1,19 @@
+/*
+ Name:		BlinkLed.h
+ Created:	13/01/2022 15:51:11
+ Author:	marco
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _BlinkLed_h
+#define _BlinkLed_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
